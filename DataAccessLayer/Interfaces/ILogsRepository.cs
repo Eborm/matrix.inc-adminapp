c585bd9c8e9d2119eab107ee0a2afe7ea4fdec9e
Eborm <bramwesel@gmail.com>
2025-06-02 16:33:50 +0200

    Log class en LogRepository Interface gemaakt

---
+++ DataAccessLayer/Interfaces/ILogsRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Models;
+
+namespace DataAccessLayer.Interfaces
+{
+    public interface ILogsRepository
+    {
+        public IEnumerable<Log> GetAllLogs();
+
+        public Log? GetLogByUID(int uid);
+
+        public void AddCustomer(Log log);
+
+        public Log? GetLogByUserName(string userName);
+
+        public Log? GetLogByAction(string action);
+    }
+}
