1f81638a764b9f43e43a2b9cbb1221712af5c94f
Eborm <bramwesel@gmail.com>
2025-06-06 12:55:15 +0200

    Added it so if the discount is over it corrects it in the database

--- KE03_INTDEV_SE_2_Base/Controllers/HomeController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/HomeController.cs
@@ -1,20 +1,36 @@
 using System.Diagnostics;
+using DataAccessLayer.Interfaces;
 using KE03_INTDEV_SE_2_Base.Models;
 using Microsoft.AspNetCore.Mvc;
+using DataAccessLayer.Models;
 
 namespace KE03_INTDEV_SE_2_Base.Controllers
 {
     public class HomeController : Controller
     {
             private readonly ILogger<HomeController> _logger;
+            private readonly IProductRepository _productRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductRepository ProductRepository)
         {
             _logger = logger;
+            _productRepository = ProductRepository;
         }
 
         public IActionResult Index()
         {
+            IEnumerable<Product> products = _productRepository.GetAllProducts();
+            foreach (var product in products)
+            {
+                if ((product.DiscountStartTime + product.DiscountDuration) < DateTime.Now.Second)
+                {
+                    product.DiscountDuration = 0;
+                    product.Discount = 0;
+                    product.DiscountStartTime = 0;
+                    _productRepository.UpdateProduct(product);
+                }
+            }
+
             return View();
         }
 
